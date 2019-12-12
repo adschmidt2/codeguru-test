abfa9e36a3aaa9461f07bb782c9aeea59df1bb6c
andreas.d.schmidt <andreas.d.schmidt@accenture.com>
2019-12-12 11:00:42 -0500

    Initial commit

---
+++ src/main/java/my/codeguru/example/codegurutest/CodeguruTestApplication.java
@@ -0,0 +1,13 @@
+package my.codeguru.example.codegurutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeguruTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeguruTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/codeguru/example/codegurutest/CodeguruTestApplicationTests.java
@@ -0,0 +1,13 @@
+package my.codeguru.example.codegurutest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeguruTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
